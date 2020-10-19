8b2914c6bacf3e0cfbfc97283695ece009281083
Samuel Meyers <thecheesecakejunkie@gmail.com>
2020-10-19 14:31:00 -0500

    Removed spag heddy code.

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
+function generateShoppingItem(item) {
+  return `<li>
+  <span class="shopping-item">${item}</span>
+  <div class="shopping-item-controls">
+    <button class="shopping-item-toggle">
+      <span class="button-label">check</span>
+    </button>
+    <button class="shopping-item-delete">
+      <span class="button-label">delete</span>
+    </button>
+  </div>
+</li>`;
+}
+
+function handleShoppingItemAdd(evt) {
+  evt.preventDefault();
+  const listItem = $('#shopping-list-entry').val();
+  $('#shopping-list-entry').val('');
+  $('.shopping-list').append(generateShoppingItem(listItem));
+}
+
+function handleShoppingItemToggle(evt) {
+  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+}
+
+function handleShoppingItemRemove(evt) {
+  $(this).closest('li').remove();
+}
+
 $(function(){
-    $('#js-shopping-list-form').submit(function(event) {
-      event.preventDefault();
-      const listItem = $('#shopping-list-entry').val();
-      $('#shopping-list-entry').val('');
-  
-      $('.shopping-list').append(
-        `<li>
-          <span class="shopping-item">${listItem}</span>
-          <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-              <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-              <span class="button-label">delete</span>
-            </button>
-          </div>
-        </li>`);
-    });
+    $('#js-shopping-list-form').submit(handleShoppingItemAdd);
 
     $('#shopping-list-entry').val('');
   
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-      $(this).closest('li').remove();
-    });
+    $('.shopping-list').on('click', '.shopping-item-delete', handleShoppingItemRemove);
   
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-      $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    });
+    $('.shopping-list').on('click', '.shopping-item-toggle', handleShoppingItemToggle);
   
   });
