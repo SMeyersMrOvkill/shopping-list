81a10f18cdd84c62b5fe01bc2c862890a74fe857
Samuel Meyers <thecheesecakejunkie@gmail.com>
2020-10-20 10:11:09 -0500

    Working rendered jQuery shopping list

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function generateShoppingItem(item) {
-  return `<li>
-  <span class="shopping-item">${item}</span>
+let store = [
+  {name: 'apples', completed: false},
+  {name: 'oranges', completed: false},
+  {name: 'milk', completed: false},
+  {name: 'bread', completed: false}
+];
+
+function generateListItem(item) {
+  let code = `<li>
+  <span class="shopping-item `;
+  if(item.completed) {
+    code += 'shopping-item__checked';
+  }
+  code += `">${item.name}</span>
   <div class="shopping-item-controls">
     <button class="shopping-item-toggle">
       <span class="button-label">check</span>
@@ -10,21 +21,41 @@ function generateShoppingItem(item) {
     </button>
   </div>
 </li>`;
+  return code;
+}
+
+function renderShoppingList() {
+  let html = '';
+  for(let item of store) {
+    html += generateListItem(item);
+  }
+  $('.shopping-list').html(html);
 }
 
 function handleShoppingItemAdd(evt) {
   evt.preventDefault();
   const listItem = $('#shopping-list-entry').val();
   $('#shopping-list-entry').val('');
-  $('.shopping-list').append(generateShoppingItem(listItem));
+  store.push({name:listItem, completed: false});
+  renderShoppingList();
 }
 
 function handleShoppingItemToggle(evt) {
-  $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  let name = $(this).closest('li').find('.shopping-item').html();
+  for(let item of store) {
+    if(item.name == name) {
+      item.completed = !item.completed;
+    }
+  }
+  renderShoppingList();
 }
 
 function handleShoppingItemRemove(evt) {
-  $(this).closest('li').remove();
+  let name = $(this).closest('li').find('.shopping-item').html();
+  store = store.filter(function(itm) {
+    return itm.name !== name;
+  });
+  renderShoppingList();
 }
 
 $(function(){
@@ -37,3 +68,7 @@ $(function(){
     $('.shopping-list').on('click', '.shopping-item-toggle', handleShoppingItemToggle);
   
   });
+  
+$(() => {
+  renderShoppingList();
+});
