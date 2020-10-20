bd6f8ee5d5befc9722ea9cd24ddb13c151bd87ff
Samuel Meyers <thecheesecakejunkie@gmail.com>
2020-10-20 10:25:17 -0500

    Added comments to our lovely code.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * Our store. Has all of the initial variables for the code.
+ */
 let store = [
   {name: 'apples', completed: false},
   {name: 'oranges', completed: false},
@@ -5,9 +8,15 @@ let store = [
   {name: 'bread', completed: false}
 ];
 
+/**
+ * Renders the shopping list item from an object. Returns rendered HTML as a string.
+ * 
+ * @param {object} item 
+ */
 function generateListItem(item) {
   let code = `<li>
   <span class="shopping-item `;
+  // Adds in the item checked class if the item has been completed, otherwise does nothing.
   if(item.completed) {
     code += 'shopping-item__checked';
   }
@@ -24,6 +33,9 @@ function generateListItem(item) {
   return code;
 }
 
+/**
+ * Calls the render function for the entire list of items, then inserts it into the document.
+ */
 function renderShoppingList() {
   let html = '';
   for(let item of store) {
@@ -32,6 +44,12 @@ function renderShoppingList() {
   $('.shopping-list').html(html);
 }
 
+/**
+ * Responds to the add item form. 
+ * Pushes a new item to the store and re-renders the list.
+ * @see renderShoppingList
+ * @param {Event} evt 
+ */
 function handleShoppingItemAdd(evt) {
   evt.preventDefault();
   const listItem = $('#shopping-list-entry').val();
@@ -40,6 +58,12 @@ function handleShoppingItemAdd(evt) {
   renderShoppingList();
 }
 
+/**
+ * Responds to the check button.
+ * Toggles the checked class on an item and re-renders the list.
+ * @see renderShoppingList
+ * @param {Event} evt 
+ */
 function handleShoppingItemToggle(evt) {
   let name = $(this).closest('li').find('.shopping-item').html();
   for(let item of store) {
@@ -50,6 +74,12 @@ function handleShoppingItemToggle(evt) {
   renderShoppingList();
 }
 
+/**
+ * Responds to the delete button.
+ * Deletes an item from the shopping list then re-renders the list.
+ * @see renderShoppingList
+ * @param {Event} evt 
+ */
 function handleShoppingItemRemove(evt) {
   let name = $(this).closest('li').find('.shopping-item').html();
   store = store.filter(function(itm) {
@@ -58,17 +88,22 @@ function handleShoppingItemRemove(evt) {
   renderShoppingList();
 }
 
+/**
+ * Links all of the functions previously listed in this document into their correct event handlers.
+ */
 $(function(){
+    // Register the add item event handler.
     $('#js-shopping-list-form').submit(handleShoppingItemAdd);
 
+    // Ensure out entry field is blank so we can see our lovely placeholder.
     $('#shopping-list-entry').val('');
   
+    // Register the delete item event handler.
     $('.shopping-list').on('click', '.shopping-item-delete', handleShoppingItemRemove);
   
+    // Register the checked item event handler.
     $('.shopping-list').on('click', '.shopping-item-toggle', handleShoppingItemToggle);
-  
+
+    // Render our list so the user can see it when the page loads.
+    renderShoppingList();
   });
-  
-$(() => {
-  renderShoppingList();
-});
